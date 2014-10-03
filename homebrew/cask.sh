3d84037e68340e88cd6436667c201334a60b13ad
dot <shuhei.kondo@gmail.com>
2014-10-03 23:55:29 +0900

    alternate brew bundle

---
+++ homebrew/cask.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# versioned
+## fonts
+brew cask install font-source-code-pro
+brew cask install font-m-plus
+
+brew cask install karabiner
+brew cask install alfred
+brew cask install iterm2
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install vlc
+brew cask install libreoffice
+brew cask install coteditor
+brew cask install brackets
+brew cask install flash
+brew cask install silverlight
+brew cask install opera
+brew cask install simple-comic
+brew cask install haroopad
+brew cask install cyberduck
+brew cask install sequel-pro
+brew cask install pg-commander
+brew cask install mysqlworkbench
+brew cask install colors
+brew cask install p4merge
+brew cask install cakebrew
+brew cask install trailer
+brew cask install nvalt
+brew cask install macdown
+brew cask install hosts
+
+# unversioned
+brew cask install dropbox --force
+brew cask install skype --force
+brew cask install github --force
+brew cask install delibar --force
+brew cask install onyx --force
+brew cask install path-finder --force
+brew cask install firefox --force
+#brew cask install hyperswitch --force
+brew cask install ibettercharge --force
+brew cask install mou --force
+brew cask install teleport --force
+brew cask install startninja --force
+brew cask install sitesucker --force
+brew cask install imageoptim --force
+brew cask install sitesucker --force
+brew cask install notational-velocity --force
+brew cask install bartender --force
+brew cask install knock --force
+brew cask install google-japanese-ime --force
+brew cask install utorrent
---
+++ homebrew/init.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+hash -r
+
+brew tap peco/peco
+brew tap homebrew/dupes
+brew tap motemen/ghq
+brew tap caskroom/fonts
+brew update
+
+brew install unzip
+brew install caskroom/cask/brew-cask
+brew install peco
+brew install ghq
+
+brew install ag
+brew install lv
+brew install colordiff
+brew install coreutils
+brew install nkf
+brew install htop-osx
+brew install pstree
+brew install ssh-copy-id
+brew install tmux
+brew install tree
+brew install reattach-to-user-namespace
+brew install wget
+brew install curl
+brew install xz
+brew install z
+brew install zsh
+brew install zsh-completions
+brew install renameutils
+brew install rlwrap
+brew install mobile-shell
+brew install jq
+brew install jsonpp
+brew install direnv
+
+brew install openssl
+brew install readline
+
+brew install git
+brew install mercurial
+brew install tig
+brew install hub
+brew install heroku-toolbelt
+
+brew install mongodb
+brew install node
+brew install phantomjs
+brew install postgresql
+brew install mysql
+brew install qt
+brew install redis
+brew install sqlite
+brew install vim
+brew install emacs --cocoa --japanese --srgb
+
+brew install dnsmasq
+
+brew install rbenv
+brew install rbenv-default-gems
+brew install rbenv-gem-rehash
+brew install rbenv-gemset
+brew install ruby-build
+
+brew install docker
+brew install boot2docker
+brew install ansible
